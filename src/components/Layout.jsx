6fdd2722f4d35a88b6270394c02f762b9889c49e
Aditya Kumar T <aditya.anil950@gmail.com>
2026-07-20 23:12:26 +0530

    feat: implement site layout component and home page structure

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+import '../pages/Home.css'
 
 export default function Layout() {
   return (
-    <div className="site">
+    <div className="mainLayoutComponent">
       <Header />
       <main className="site-main">
         <Outlet />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import { profile } from '../data/profile'
-import './Home.css'
 
 export default function Home() {
   const { name, title, location, summary, credentials } = profile;
